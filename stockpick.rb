88e65c8c10d88b8e7481be1edf5566c76e561ba2
Carlos E <58950500+CarlosElizalde98@users.noreply.github.com>
2024-02-07 14:02:36 -0600

    added logic for stockpicking

--- stockpick.rb
+++ stockpick.rb
@@ -1,2 +1,23 @@
-def stockpick(stocks)
+def stock_pick(stocks)
+  stockDays = []
+  min_price = stocks[0]
+  min_index = 0
+
+  profit = 0
+  days = [0,0]
+stocks.each_with_index do |stock, index|
+  if stock < min_price
+      min_price = stock
+      min_index = index
+  end
+  if stock - min_price > profit
+    profit = stock - min_price
+    days = [min_index, index]
 end
+
+end
+
+
+end
+
+stock_pick([17,3,6,9,15,8,6,1,10])
